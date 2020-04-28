a7eb784dc9adc55f7f3d69dbf5070d407c9d1f8e
sq <1102049116@qq.com>
2020-04-28 10:29:12 +0800

    个人文档

---
+++ src/test/java/com/sq/doc/DocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sq.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
